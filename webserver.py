d7d32262a0736a969d72bda5b918125ee0152404
asimshrestha2 <asims178@gmail.com>
2016-11-20 16:05:26 -0500

    Added a try/except

--- webserver.py
+++ webserver.py
@@ -10,19 +10,31 @@ print 'Serving HTTP on port %s ...' % PORT
 while True:
     client_connection, client_address = listen_socket.accept()
     request = client_connection.recv(1024)
+    print request
     req = request.split("\r\n")
     protocal = req[0].split(" ")
     filename = protocal[1]
-    if(filename == "/"):
-        rf = open('./serverfiles/index.html', 'r')
-    else:
-        rf = open('./serverfiles' + filename, 'r')
-    
-    filecontent = rf.read()
-    http_response = """\
+    try:
+        if(filename == "/"):
+            rf = open('./serverfiles/index.html', 'r')
+        else:
+            rf = open('./serverfiles' + filename, 'r')
+
+        filecontent = rf.read()
+        http_response = """\
 HTTP/1.1 200 OK
 
 %s
 """
+        pass
+
+    except Exception as e:
+        filecontent = ""
+        http_response = """\
+HTTP/1.1 404 Not Found
+
+%s
+"""
+
     client_connection.sendall(http_response  % (filecontent))
     client_connection.close()
