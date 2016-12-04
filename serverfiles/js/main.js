45953b37ab687565752c2f99f2bec75f5b452e33
asimshrestha2 <asims178@gmail.com>
2016-12-04 15:25:51 -0500

    fix player 2 not showing

--- serverfiles/js/main.js
+++ serverfiles/js/main.js
@@ -1,11 +1,11 @@
+var playernum = 0;
+
 window.onload = function() {
   var room = prompt("Please enter your room number!");
   console.log(room);
 
-  var player = postRequestURL("/newroom", "room=" + room);
-  var playernum = player.split("=")[1];
-  document.getElementById('playernum').innerHTML = "You are player: " + playernum;
-
+  var player = postPlayerNum("/newroom", "room=" + room);
+  /*
   document.getElementById('rock').addEventListener("click", function() {
     postRequestURL("/reply", "room=" + room + "&player=" + playernum + "&item=1");
   });
@@ -22,13 +22,39 @@ window.onload = function() {
     if(winnernum != 0 && winnernum !== undefined){
       document.getElementById('winner').innerHTML = "The Winner: " + winnernum;
     }
-  }, 100);
-
-}
+  }, 1000);
+  */
+};
 
 window.onbeforeunload = function () {
     postRequestURL("/playerleaves", "room=" + room + "&player=" + playernum);
 };
+
+function postPlayerNum(url, body) {
+  url = 'http://' + document.location.hostname + ':' + document.location.port + url
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', url, true);
+  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  //'room=1234&player=1&item=0'
+  xhr.send(body);
+  var result = "";
+  xhr.onload = function () {
+    // do something to response
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        result = xhr.responseText;
+        console.log(result.split("="));
+        playernum = result.split("=")[1];
+        if(playernum == -1){
+          document.getElementById('playernum').innerHTML = "Room is full!";
+        } else {
+          document.getElementById('playernum').innerHTML = "You are player: " + playernum;
+        }
+      }
+    }
+  };
+}
+
 
 function postRequestURL(url, body) {
   url = 'http://localhost:8888' + url
@@ -44,8 +70,8 @@ function postRequestURL(url, body) {
       if (xhr.status === 200) {
         result = xhr.responseText;
         console.log(result);
+        return result;
       }
     }
   };
-  return result;
 }
